8b454795ce0acca539d3f67358e5043c69b1164f
WeaverArtiz <spawlo112@gmail.com>
2025-06-30 14:58:36 +0200

    Pong development
    Added pong gameplay:
    bouncing ball, ball acceleration, flipper acceleration,
    retrieving ball upon it "scoring", randomizing start.

--- src/Pong.cpp
+++ src/Pong.cpp
@@ -16,10 +16,9 @@ extern SDL_Renderer* Renderer;
 SDL_Texture* boinkT = nullptr;
 SDL_Texture* ballT = nullptr;
 
-void moveBall(float speed, float& x, float& y, float& dir_x, float& dir_y, float left_y, float right_y){
+void moveBall(float& speed, float& x, float& y, float& dir_x, float& dir_y, float left_y, float right_y, float& fspeed){
   x += speed * dir_x;
   y += speed * dir_y;
-  
   // side bouncing
   if(y<40 || y>680){
     y-= speed * dir_y;
@@ -27,12 +26,22 @@ void moveBall(float speed, float& x, float& y, float& dir_x, float& dir_y, float
   }
   
   //simple boinker bounce (todo: speed acceleration | relative angle bounce)
-  if((x<67 && y>left_y - 16 && y<left_y + 16) || (x>1013 && x>right_y - 16 && y<right_y+ 16)){
+  if(x>62 && x<67 && y>left_y - 48 && y<left_y + 48){
     x-= speed * dir_x;
-    dir_x = -dir_x;
-    // also it causes funny stuff rn - ball may go inside boinker causing weird effects
+    dir_y = (y - left_y)  / 48;
+    dir_x = std::sqrt(1 - dir_y*dir_y);
+    x += speed * dir_x;
+    speed += 0.003;
+    fspeed += 0.003;
   }
-
+  else if(x>1013 && x<1018 && y>right_y - 48 && y<right_y + 48){
+    x-= speed * dir_x;
+    dir_y = (y - right_y)  / 48;
+    dir_x = - std::sqrt(1 - dir_y*dir_y);
+    x += speed * dir_x;
+    speed += 0.003;
+    fspeed += 0.003;
+  }
   if(x > 1096 || x < -16) y = -500;
 }
 
@@ -60,11 +69,14 @@ void Pong(){
   SDL_zero(e);
   const bool* inputs = SDL_GetKeyboardState(NULL);
 
-  float left_y = 360, right_y = 360, ball_x= 540, ball_y=360, dir_x=0.4, dir_y=0.6;
+  float left_y = 360, right_y = 360, ball_x= 540, ball_y=360, dir_x, dir_y, speed=0.02, fspeed=0.03;
+  bool left_start = false;
   boinkT = IMG_LoadTexture(Renderer, "../assets/imgs/boinker.png");
   ballT = IMG_LoadTexture(Renderer, "../assets/imgs/ball.png");
   if(ballT == NULL || boinkT == NULL)std::cout<<"Error loading texture: "<<SDL_GetError()<<"\n";
 
+  dir_x = (float)(rand()%50 + 51) / 100;
+  dir_y = std::sqrt(1 - dir_x * dir_x);
   while(!quit){ 
     while(SDL_PollEvent(&e)){
       if(e.type == SDL_EVENT_KEY_DOWN){
@@ -75,20 +87,24 @@ void Pong(){
         }
       }        
     }
-    moveBall(0.08, ball_x, ball_y, dir_x, dir_y, left_y, right_y);
+    moveBall(speed, ball_x, ball_y, dir_x, dir_y, left_y, right_y, fspeed);
     if(ball_y == -500){
       //if(x<0) right scored
       //else left scored
       //add some function waiting few secs and showing score
+      left_start = !left_start;
       ball_x = 540;
       ball_y = 360;
       dir_x = (float)(rand()%50 + 51) / 100;
       dir_y = std::sqrt(1 - dir_x * dir_x);
+      if(left_start) dir_x = - dir_x;
+      speed = 0.02;
+      fspeed = 0.03;
     };
-    if(inputs[SDL_SCANCODE_W] && left_y > 40) left_y-=0.03;
-    if(inputs[SDL_SCANCODE_S] && left_y < 680) left_y+=0.03;
-    if(inputs[SDL_SCANCODE_UP] && right_y > 40) right_y-=0.03;
-    if(inputs[SDL_SCANCODE_DOWN] && right_y < 680) right_y+=0.03;
+    if(inputs[SDL_SCANCODE_W] && left_y > 40) left_y-=fspeed;
+    if(inputs[SDL_SCANCODE_S] && left_y < 680) left_y+=fspeed;
+    if(inputs[SDL_SCANCODE_UP] && right_y > 40) right_y-=fspeed;
+    if(inputs[SDL_SCANCODE_DOWN] && right_y < 680) right_y+=fspeed;
 
     PongRenderFrame(left_y, right_y, ball_x, ball_y);
   }
